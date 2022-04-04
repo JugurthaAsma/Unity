f08d3b880772c16b497d58dc7174b3e5fcc78db7
Jugurtha <jugurtha.1703@gmail.com>
2022-04-04 12:23:22 +0100

    update level bars

--- Life Manager/Assets/Scripts/LevelBar.cs
+++ Life Manager/Assets/Scripts/LevelBar.cs
@@ -9,9 +9,11 @@ public class LevelBar : MonoBehaviour
     public Sprite sprite;
     public Color color;
     public string text;
+    public float damage;
     Image fillComponent;
     Image iconComponent;
     Text textComponent;
+    
     
 
     // start
@@ -23,7 +25,7 @@ public class LevelBar : MonoBehaviour
     // update
     void Update()
     {
-        //setLevel(slider.value - 0.1f);
+        takeDamage(damage);
     }
 
 
@@ -36,8 +38,8 @@ public class LevelBar : MonoBehaviour
     public void setLevel(float level)
     {
         slider.value = level;
-        textComponent.text = text + " " + slider.value.ToString();
-        Debug.Log("value: " + slider.value);
+        textComponent.text = text + " " + slider.value.ToString("F1") + " %";
+        //Debug.Log("value: " + slider.value);
     }
 
     void setChildren()
@@ -71,76 +73,14 @@ public class LevelBar : MonoBehaviour
 
 
 
-        /*
-        // list of all children
-        Transform[] children = GetComponentsInChildren<Transform>();
-         
-        // foreach loop through all children
-        foreach (Transform child in children)
-        {
-            // if child is the Fill
-            if (child.gameObject.name == "Fill")
-            {
-                child.gameObject.sprite = sprite;
-                Debug.Log(child.gameObject.sprite);
-                //slider = child.gameObject.GetComponent<Slider>();
-            }
-            // if child is a sprite
-            else if (child.gameObject.name == "Sprite")
-            {
-                sprite = child.gameObject.GetComponent<Image>().sprite;
-            }
-            // if child is a text
-            else if (child.gameObject.name == "Text")
-            {
-                text = child.gameObject.GetComponent<Text>().text;
-            }
-            // if child is a color
-            else if (child.gameObject.name == "Color")
-            {
-                color = child.gameObject.GetComponent<Image>().color;
-            }
-        }
-        /*
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            // get child
-            Transform child = transform.GetChild(i);
-
-            // set sprite
-            if (child.name == "Sprite")
-            {
-                child.sprite = sprite;
-                Debug.Log(child.sprite);
-            }
-
-            // set color
-            if (child.name == "Color")
-            {
-                child.color = color;
-                Debug.Log(child.color);
-            }
-
-            // set text
-            if (child.name == "Text")
-            {
-                child.text = text;
-            }
-        }
-/*
-        // get child named "Image" and set it's sprite to sprite
-        Image image = transform.Find("Image").GetComponent<Image>();
-        image.sprite = sprite;
-        Debug.Log(image.sprite);
-
-
-        // get child named "Fill" and set it's color to color
-        Image fill = transform.Find("Fill").GetComponent<Image>();
-        fill.color = color;
-        Debug.Log(fill.color);
-
-        ;*/
      }
+
+    public void takeDamage(float damage)
+    {
+        
+        //Debug.Log("damage: " + damage);
+        setLevel(slider.value - damage);
+    }
 
   
 }
