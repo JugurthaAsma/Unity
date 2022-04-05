73a53c515258a72e4a0a5d7760bbaa158db5a6ac
hylionx <hylia.boudahba@gmail.com>
2022-04-05 18:02:10 +0200

    pause menu

---
+++ Life Manager/Assets/Scripts/Pause.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    [SerializeField] GameObject PauseMenu;
+
+
+    private void Start()
+    {
+        ResumeGame();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //si jappuie sur espace 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+        
+    }
+
+
+    
+     public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        //le temps reste le meme
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+   void PauseGame()
+    {   
+        PauseMenu.SetActive(true);
+        //le temps sarrete
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
