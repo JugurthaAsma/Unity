66e83b2ff1fde022bb2800226a255e8e17298fc2
maximax64 <61144909+maximax64@users.noreply.github.com>
2022-04-07 01:05:45 +0200

    onglet de base

---
+++ Life Manager/Assets/Scripts/boutonTab.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class boutonTab : MonoBehaviour
+{
+    public GameObject bouton1;
+    public GameObject bouton2;
+
+    public GameObject content1;
+    public GameObject content2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showContent1()
+    {
+        content1.SetActive(true);
+        content2.SetActive(false);
+        bouton1.GetComponent<Button>().image.color = new Color32(255, 255, 255, 255);
+        bouton2.GetComponent<Button>().image.color = new Color32(212, 212, 212, 255);
+    }
+    public void showContent2()
+    {
+        content1.SetActive(false);
+        content2.SetActive(true);
+
+        bouton2.GetComponent<Button>().image.color = new Color32(255, 255, 255, 255);
+        bouton1.GetComponent<Button>().image.color = new Color32(212, 212, 212, 255);
+
+    }
+}
