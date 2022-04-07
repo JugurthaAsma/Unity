0665f4d61e69c51c8cb864ddc9042f9d48933a5c
hylionx <hylia.boudahba@gmail.com>
2022-04-07 22:05:42 +0200

    little script update

--- Life Manager/Assets/Scripts/Item.cs
+++ Life Manager/Assets/Scripts/Item.cs
@@ -14,6 +14,9 @@ public class Item : MonoBehaviour
     public LevelBar levelBarToDamage;
     public int damageEffect;
 
+    Transform buttonChild;
+    Transform textChild;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,45 +26,55 @@ public class Item : MonoBehaviour
 
     void SetChildren()
      {
+
         foreach (Transform child in transform)
         {
             // the icon on the button
             if (child.name == "Button")
             {
-                setIcon(child);
+                setButton(child);
             }
             // if child is the Name
             else if (child.name == "Name")
             {
-                setName(child);
+                setText(child);
             }
         }
      }
 
-    void setName(Transform child)
+    void setText(Transform child)
     {
-        child.GetComponent<Text>().text = quantity + " " + itemName;
-        //Debug.Log(child.GetComponent<Text>().text);
+        textChild = child;
+        UpdateText();
+        
 
     }
 
-    void setIcon(Transform child)
+    void setButton(Transform child)
     {
-
+        buttonChild = child;
         // set the image of button
-        child.GetComponent<Image>().sprite = icon;
+        buttonChild.GetComponent<Image>().sprite = icon;
         //Debug.Log(icon.sprite);
     }
 
-    public void applyLevelBarsEffect()
+    public void ApplyLevelBarsEffect()
     {
 
         if (quantity > 0)
         {
+            quantity--;
+            UpdateText();
             levelBarToHeal.Heal(healEffect);
             levelBarToDamage.TakeDamage(damageEffect);
-            quantity--;
+            
         }
+    }
+
+    private void UpdateText()
+    {
+        textChild.GetComponent<Text>().text = quantity + " " + itemName;
+        //Debug.Log(textChild.GetComponent<Text>().text);
     }
 }
 
